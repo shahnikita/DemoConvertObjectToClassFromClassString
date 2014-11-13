f1f4ed1dd6e678ba5087ea4b6348aeb03f71aea4
shahnikita <shahnikita.1991@yahoo.com>
2014-11-13 15:10:00 +0530

    Added my project

---
+++ DemoConvertObjectToClassFromClassString/Path.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConvertObjectToClassFromClassString
+{
+    public class Path
+    { 
+       // This property defines type of the shape
+        /// </summary>
+       // [JsonProperty("se")]
+        public string SvgElement { get; set; }
+
+        /// <summary>
+        /// this property defines svg root id 
+        /// </summary>
+       // [JsonProperty("sId")]
+        public string SvgRootId { get; set; }
+        /// <summary>
+        /// A set of commands which define the path
+        /// </summary>
+       // [JsonProperty("d")]
+        public string Data { get; set; }
+        /// <summary>
+        /// The path will be scaled so that the computed path length of the points equals this value
+        /// </summary>
+       // [JsonProperty("pl")]
+        public int PathLength { get; set; }
+        // <summary>
+        /// The fill property defines the name of the color or color code to be filled for the element
+        /// </summary>
+       // [JsonProperty("fc")]
+        public string FillColor { get; set; }
+        /// <summary>
+        /// This property defines the border color of the element
+        /// </summary>
+       // [JsonProperty("s")]
+        public string Stroke { get; set; }
+
+        /// <summary>
+        /// The stroke-width property defines the thickness of a line, text or outline of an element
+        /// </summary>
+       // [JsonProperty("sw")]
+        public string StrokeWidth { get; set; }
+
+    }
+}
---
+++ DemoConvertObjectToClassFromClassString/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConvertObjectToClassFromClassString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dynamic shape =	new {
+            //    SvgElement = 'path',
+            //    SvgRootId = 'page1',
+            //    Data = 'M145 224 L146 223 L148 221 L149 219 L152 217 ',
+            //    FillColor = 'none',
+            //    Stroke = 'black'
+            //};
+           var json="{   'sId': 'page1',  'd': 'M145 224 L146 223 L148 221 L149 219 L152 217 L159 213 L161 211 L166 209 L169 206 L175 203 L178 202 L183 200 L186 198 L188 197 L191 196 L195 194 L196 193 L199 192 L200 191 L201 190 L202 190 L203 190 L204 190 L205 190 L205 189 ',  'fc': 'none',  's': 'black'}";
+            var jOBject=JsonConvert.DeserializeObject(json) as JObject;
+
+            Type type = Assembly.GetExecutingAssembly().GetTypes().First(t => t.Name == "Path");
+            var x = jOBject.ToObject(type);
+        }
+    }
+}
